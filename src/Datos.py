87f3cef4baab7e50539ba1081c9c9a17cd632976
FelipeGomezP <140621017+FelipeGomezP@users.noreply.github.com>
2024-11-08 18:53:15 -0500

    Update Datos.py
    Función 5

--- src/Datos.py
+++ src/Datos.py
@@ -25,6 +25,18 @@ def count_chars(filename):
     with open(filename,'r') as file: 
         text = file.read().strip()  
         print(len(text)) 
+
+def calcular_estadisticas(filename, columna): 
+    with open(filename,'r') as file: 
+        reader = csv.reader(file) 
+        headers = next(reader) 
+        index = headers.index(columna) 
+        data = [] 
+        for row in reader: 
+            data.append(float(row[index])) 
+        print('Promedio:', sum(data)/len(data))
+        print('Maximo:', max(data)) 
+        print('Minimo:', min(data))
 def main():
     #Tu código va aquí. Mantén la indentación
     pass # borra esta línea cuando con inicies tu código
