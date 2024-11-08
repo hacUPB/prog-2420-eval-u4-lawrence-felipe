5eb522524ddf61ebf10d2539b0ff33c745071b07
FelipeGomezP <140621017+FelipeGomezP@users.noreply.github.com>
2024-11-08 17:44:46 -0500

    Update Datos.py
    función 3

--- src/Datos.py
+++ src/Datos.py
@@ -10,6 +10,16 @@ def count_words(filename):
         palabras = file.read().split()  
         print(len(palabras)) 
 
+def replace_word(filename, palabra1, palabra2):
+    with open(filename,'r') as file:
+        data = file.read() 
+        data = data.replace(palabra1, palabra2) 
+    
+    with open(filename,'w') as file: 
+        file.write(data) 
+    
+    with open(filename,'r') as file:
+        print(file.read())
 def main():
     #Tu código va aquí. Mantén la indentación
     pass # borra esta línea cuando con inicies tu código
