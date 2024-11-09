9f6e2c39cfba66388d88993c0f44f38adca495d4
Lawr45 <lawrence.barrientosm.col@upb.edu.co>
2024-11-08 23:51:50 -0500

    Parte 7
    Interfaz

--- src/Datos.py
+++ src/Datos.py
@@ -52,8 +52,38 @@ def main():
          plt.plot(data) 
          plt.show() 
  
- 
-     pass 
+ def main():
+    while True:
+        print('Bienvenido!')
+        print('\n')
+        print('1. Ver archivos en la ruta actual')
+        print('2. Archivo de texto (.txt)')
+        print('3. Archivo CSV (.csv)')
+        print('4. Salirse del programa')
+        option = int(input('Por favor seleccione una opcion:'))
+
+        if option == 1:
+            list_files()
+        elif option == 2:
+            filename = input('Ingrese el nombre del archivo (.txt):  ')
+            print('\n')
+            print('1. Contar numero de palabras')
+            print('2. Reemplazar una palabra por otra')
+            print('3. Contar el numero de caracteres')
+            option2 = int(input('Por favor seleccione una opcion:  '))
+
+            if option2 == 1:
+                count_words(filename)
+            elif option2 == 2:
+                palabra1 = input('Por favor escriba la palabra que desea reemplazar:  ')
+                palabra2 = input('Por favor escriba la palabra por la que quiere reemplazar:  ')
+                replace_word(filename, palabra1, palabra2)
+            elif option2 == 3:
+                count_chars(filename)
+            else:
+                print('Opcion no valida')
+             
+ pass 
  
 
 if __name__ == "__main__":
