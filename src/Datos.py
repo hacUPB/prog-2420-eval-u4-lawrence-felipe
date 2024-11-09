e43a0f14dc9c83f473574302ed73d230cc6fcdc7
Lawr45 <lawrence.barrientosm.col@upb.edu.co>
2024-11-08 22:48:29 -0500

    Parte 6
    funcion que toma como argumento el nombre del archivo y el nombre de la columna

--- src/Datos.py
+++ src/Datos.py
@@ -1,46 +1,60 @@
-import os
-import csv
-import matplotlib.pyplot as plt
- 
-def list_files():
-    print(os.listdir()) 
-
-def count_words(filename): 
-    with open(filename,'r') as file: 
-        palabras = file.read().split()  
-        print(len(palabras)) 
-
-def replace_word(filename, palabra1, palabra2):
-    with open(filename,'r') as file:
-        data = file.read() 
-        data = data.replace(palabra1, palabra2) 
-    
-    with open(filename,'w') as file: 
-        file.write(data) 
-    
-    with open(filename,'r') as file:
-        print(file.read())
-
-def count_chars(filename): 
-    with open(filename,'r') as file: 
-        text = file.read().strip()  
-        print(len(text)) 
-
-def calcular_estadisticas(filename, columna): 
-    with open(filename,'r') as file: 
-        reader = csv.reader(file) 
-        headers = next(reader) 
-        index = headers.index(columna) 
-        data = [] 
-        for row in reader: 
-            data.append(float(row[index])) 
-        print('Promedio:', sum(data)/len(data))
-        print('Maximo:', max(data)) 
-        print('Minimo:', min(data))
 def main():
-    #Tu código va aquí. Mantén la indentación
-    pass # borra esta línea cuando con inicies tu código
 
+ import os
+ import csv
+ import matplotlib.pyplot as plt
+  
+ def list_files():
+     print(os.listdir()) 
+ 
+ def count_words(filename): 
+     with open(filename,'r') as file: 
+         palabras = file.read().split()  
+         print(len(palabras)) 
+ 
+ def replace_word(filename, palabra1, palabra2):
+     with open(filename,'r') as file:
+         data = file.read() 
+         data = data.replace(palabra1, palabra2) 
+     
+     with open(filename,'w') as file: 
+         file.write(data) 
+     
+     with open(filename,'r') as file:
+         print(file.read())
+ 
+ def count_chars(filename): 
+     with open(filename,'r') as file: 
+         text = file.read().strip()  
+         print(len(text)) 
+ 
+ def calcular_estadisticas(filename, columna): 
+     with open(filename,'r') as file: 
+         reader = csv.reader(file) 
+         headers = next(reader) 
+         index = headers.index(columna) 
+         data = [] 
+         for row in reader: 
+             data.append(float(row[index])) 
+         print('Promedio:', sum(data)/len(data))
+         print('Maximo:', max(data)) 
+         print('Minimo:', min(data))
+ 
+ def graficar_columna(filename, columna): 
+     with open(filename,'r') as file: 
+         reader = csv.reader(file) 
+         headers = next(reader) 
+         index = headers.index(columna) 
+         data = [] 
+         for row in reader: 
+             data.append(float(row[index])) 
+        
+         plt.plot(data) 
+         plt.show() 
+ 
+ 
+     pass 
+ 
 
 if __name__ == "__main__":
     main()
